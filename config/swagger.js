eaab81a773a194e1b3748f0f895b15276c54f62a
Maksym Volik <maksymvolik@gmail.com>
2024-02-12 17:45:12 +0200

    addet static swagger-ui.css

--- config/swagger.js
+++ config/swagger.js
@@ -22,10 +22,7 @@ const options = {
     apis: ["./routes/*.js", "./models/*.js"],
 };
 
-const swaggerSpec = swaggerJSDoc(options, {
-    customCssUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.3/swagger-ui.min.css",
-});
+const swaggerSpec = swaggerJSDoc(options);
 
 const swaggerDocs = (app) => {
     app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
