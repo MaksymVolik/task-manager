be8b311c216b9e0d823a10d2b329ca760ed7b3c6
Maksym Volik <maksymvolik@gmail.com>
2024-02-13 13:11:11 +0200

    Changed enveroment

--- config/db.js
+++ config/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: [".env.local", ".env"] });
 
+if (!process.env.MONGODB_URI) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
 const URI = process.env.MONGODB_URI;
 
 mongoose
